56f46e1b4d8cfbc0cdd5344c27b612f752127dcb
Ponhagen <pontus.havmyr@stud.hkr.se>
2025-05-07 12:40:46 +0200

    Minor changes

--- server/routes/assignmentRoutes.js
+++ server/routes/assignmentRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Assignment = require('../models/assignmentRoutes');
+const Assignment = require('../models/ProjectAssignment');
 
 router.post('/', async (req, res) => {
     const { employee_id, project_code, start_date } = req.body;
