fbc0b623fd19a1cced723f19da318f069f3aea6e
Ponhagen <pontus.havmyr0009@stud.hkr.se>
2025-05-27 10:04:55 +0200

    Minor changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Header } from './components/header';
+import { Header } from './components/Header';
 import { AssignmentForm } from './components/AssignmentForm';
 import { AssignmentTable } from './components/AssignmentTable';
 
