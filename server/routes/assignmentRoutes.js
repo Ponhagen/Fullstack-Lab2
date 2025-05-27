b01232930165ab97e30c5c64dce2e9e7ded5b3ce
Ponhagen <pontus.havmyr0009@stud.hkr.se>
2025-05-27 09:57:40 +0200

    Minor changes

--- server/routes/assignmentRoutes.js
+++ server/routes/assignmentRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Assignment = require('../models/ProjectAssignment');
+const Assignment = require('../models/projectAssignment');
 
 
 // Get function to fetch all assignments
