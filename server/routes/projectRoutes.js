4b370a0935c7b4ffcc917ebbbc4f6cfd53587a0d
Ponhagen <pontus.havmyr0009@stud.hkr.se>
2025-05-27 09:58:51 +0200

    Minor changes

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Project = require('../models/Project');
+const Project = require('../models/project');
 
 // Get function to fetch all projects
 router.get('/', async (req, res) => {
